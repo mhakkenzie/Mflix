ca19c7adf91d9ae80fadae35dfcaf2ba3a7d1654
mhakkenzie <mhakkenzieafunggol@gmail.com>
2025-06-09 15:49:10 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '5b0e4a28b2ed3f30506edd7002b64e39';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
